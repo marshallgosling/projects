a180df0a54ad645a5f0990e6d983bbd7c576a786
gaoqi <gaoqi@192.168.1.3>
2022-12-05 21:10:57 +0800

    linux sdk sample

--- Linux/samples/Agora-RTM-Auction-Java/src/main/java/io/agora/auction/AuctionMain.java
+++ Linux/samples/Agora-RTM-Auction-Java/src/main/java/io/agora/auction/AuctionMain.java
@@ -1,19 +1,32 @@
 package io.agora.auction;
 
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Properties;
 import java.util.Scanner;
+import java.io.FileOutputStream;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+// class CONFIG {
+//     public static final String APP_ID = "b751e95bce6c41dea55562cb027f0c61";
+//     public static final String USER_ID = "auction_admin_user";
+//     public static final String SERVER_IP = "192.168.1.3";
+//     public static final String REDIS_IP = "192.168.1.3";
+//     public static final String REDIS_KEY = "AUCTION_CHANNEL_";
+//     public static final String BASE_URL = "http://192.168.1.3/";
+//     public static final String LOG_DIR = "/data/agora/logs/";
+// }
+
 class CONFIG {
     public static final String APP_ID = "b751e95bce6c41dea55562cb027f0c61";
     public static final String USER_ID = "auction_admin_user";
-    public static final String SERVER_IP = "192.168.1.9";
-    public static final String REDIS_IP = "192.168.1.9";
+    public static final String SERVER_IP = "192.168.1.3";
+    public static final String REDIS_IP = "127.0.0.1";
     public static final String REDIS_KEY = "AUCTION_CHANNEL_";
-    public static final String BASE_URL = "http://192.168.1.9/";
+    public static final String BASE_URL = "http://www.yystory.co/";
+    public static final String LOG_DIR = "/data/agora/logs/";
 }
 
 public class AuctionMain {
@@ -49,7 +62,17 @@ public class AuctionMain {
 
     public static void main(String[] args) {
 
-        HashMap<String, AuctionThread> threads = new HashMap<String, AuctionThread>();
+
+        String f = "setting.properties";
+        Properties props = new Properties();
+        try {
+            props.load(new java.io.FileInputStream(f));
+        }catch(Exception e)
+        {
+
+        }
+ 
+        ArrayList<AuctionThread> threads = new ArrayList<AuctionThread>();
 
         AuctionMain main = new AuctionMain();
 
@@ -60,13 +83,28 @@ public class AuctionMain {
         for(int i=0;i<channels.length;i++)
         {
             String channelId = channels[i];
-            System.out.println("Init thread with channelid: "+channelId);          
-            
+ 
             AuctionThread at = new AuctionThread(CONFIG.APP_ID, CONFIG.USER_ID, channelId);
-
+            
+            threads.add(at);
             at.start();
+            try {
+                at.join();
+            }
+            catch(Exception err)
+            {
+                System.out.println(err.getMessage());
+            }
+            
+        }
+        
+        while (true) {
+           
+            for (AuctionThread thread : threads) {
+                 thread.checkMetadata();          
+            }
+           
 
-            threads.put(channelId, at);
         }
     }
 }
--- Linux/samples/Agora-RTM-Auction-Java/src/main/java/io/agora/auction/AuctionThread.java
+++ Linux/samples/Agora-RTM-Auction-Java/src/main/java/io/agora/auction/AuctionThread.java
@@ -4,9 +4,18 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.text.SimpleDateFormat;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.FileHandler;
+import java.util.logging.Formatter;
+import java.util.logging.LogRecord;
+import java.util.logging.Logger;
 
 import java.util.List;
+import java.util.Date;
 import java.util.Map;
+
 import io.agora.rtm.RtmClient;
 import io.agora.rtm.RtmClientListener;
 import io.agora.rtm.RtmMessage;
@@ -22,6 +31,20 @@ import io.agora.rtm.RtmStatusCode;
 import io.agora.rtm.RtmChannelAttribute;
 
 import redis.clients.jedis.Jedis;
+
+class MyFormatter extends Formatter {
+
+    private SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-M-dd hh:mm:ss");
+
+    @Override
+    public String format(LogRecord record) {
+        
+        return "["+simpleDateFormat.format(new Date(record.getMillis()))+"] local."
+            +record.getLevel().getName()+":"
+            +record.getMessage()+"\n";
+    }
+
+}
 
 public class AuctionThread extends java.lang.Thread {
 
@@ -39,13 +62,77 @@ public class AuctionThread extends java.lang.Thread {
 
     private boolean exitCode = false;
     private ChannelListener channelListener;
+    private Logger logger;
+    private boolean ready = false;
+
+    private String key;
 
     public AuctionThread(String appid, String userid, String channelid)
     {
         appId = appid;
         userId = userid;
         channelId = channelid;
+        key = CONFIG.REDIS_KEY + channelId;
+        initLog();
 
+        try {
+            jedis = new Jedis(CONFIG.REDIS_IP, 6379);
+            jedis.select(6);
+            //jedis.auth(redis_auth);
+        }
+        catch (Exception e) {
+            writeLog("Redis auth error!", Level.INFO);
+            
+            throw new RuntimeException("Need to check redis password");
+        }
+        try {
+            mRtmClient = RtmClient.createInstance(appId, new ClientListener());
+            writeLog("Rtm sdk init succeed!", Level.INFO);
+        }
+        catch (Exception e) {
+            writeLog("Rtm sdk init fatal error!", Level.INFO);
+            throw new RuntimeException("Need to check rtm sdk init process");
+        }
+
+        writeLog("Init thread with channelid: "+channelId, Level.INFO);   
+
+    }
+
+    private void initLog()
+    {
+        try{
+            //日志记录器
+            
+            logger = Logger.getLogger("agora-"+channelId);
+            
+            //日志处理器
+            
+            FileHandler fileHandler = new FileHandler(CONFIG.LOG_DIR + "channel_" + channelId + ".log");
+        
+            fileHandler.setFormatter(new MyFormatter());
+            
+            //注册处理器          
+            logger.addHandler(fileHandler);
+                    
+        }
+        catch(IOException err)
+        {
+            
+            logger = null;
+        }
+    }
+
+    private void writeLog(String msg, Level level )
+    {
+        if(logger != null)
+        {
+            //记录日志消息
+            //需要记录的日志消息
+            LogRecord lr = new LogRecord(level, msg);
+            logger.log(lr);
+        }
+
+        //System.out.println(msg);
     }
 
     public void exitCode()
@@ -53,87 +140,80 @@ public class AuctionThread extends java.lang.Thread {
         exitCode = true;
     }
 
+    public boolean isReady(){
+        return ready;
+    }
+
     @Override
     public void run() {
 
-        try {
-            jedis = new Jedis(CONFIG.REDIS_IP, 6379);
-            //jedis.auth(redis_auth);
-        }
-        catch (Exception e) {
-            System.out.println("Redis auth error!");
-            throw new RuntimeException("Need to check redis password");
-        }
-        try {
-            mRtmClient = RtmClient.createInstance(appId, new ClientListener());
-            System.out.println("Rtm sdk init succeed!");
-        }
-        catch (Exception e) {
-            System.out.println("Rtm sdk init fatal error!");
-            throw new RuntimeException("Need to check rtm sdk init process");
-        }
+        if (!ready) {
+            String token = getToken();
 
-        String token = getToken();
-
-        System.out.println("Rtm login start!");
-        login(token);
-        try {
-            sleep(1000);
-        }
-        catch (Exception e) {
-           
-            throw new RuntimeException("sleep error");
-        }
-
-        System.out.println("Rtm join channel start!");
-        joinChannel();
-        
-        try {
-            sleep(1000);
-        }
-        catch (Exception e) {
-            
-            throw new RuntimeException("sleep error");
-        }
-
-        
-        if (loginStatus && channelStatus) {
-            String data;
-            String key = CONFIG.REDIS_KEY + channelId;
-    
-            System.out.println("Ready for Redis key metadata:"+key);
+            writeLog("Rtm login start!", Level.INFO);
+            login(token);
             try {
-                while(!exitCode)
-                {
-                    if (jedis.exists(key)) {
-                        data = jedis.get(key);
-
-                        if (data.length() == 0)
-                        {
-                            continue;
-                        }
-            
-                        jedis.set(key, "");
-
-                        System.out.println("Read Metadata from Redis: "+data);
-            
-                        sendChannelMetadata("auction", data);
-                    }
-                    else {
-                        sleep(100);
-                    }
-                    
-                }
+                sleep(1000);
             }
             catch (Exception e) {
             
-                throw new RuntimeException("Redis function error."+e.getMessage());
+                throw new RuntimeException("sleep error");
             }
-    
-        }
 
+            writeLog("Rtm join channel start!", Level.INFO);
+            joinChannel();
+            
+            try {
+                sleep(1000);
+            }
+            catch (Exception e) {
+                
+                throw new RuntimeException("sleep error");
+            }
+
+            if (loginStatus && channelStatus) {
+                ready = true;
+                syncFirstTimeMetadata();
+
+                try {
+                    sleep(1000);
+                }
+                catch (Exception e) {
+                    
+                    throw new RuntimeException("sleep error");
+                }
+
+                writeLog("Ready for sync Redis key & metadata:"+key, Level.INFO);
+            }
+        }
         
-        logout();
+    
+    }
+
+    public void checkMetadata() {
+        if( !ready ) {
+            try {
+                sleep(100);
+            }
+            catch (Exception e) {
+                
+                throw new RuntimeException("sleep error");
+            }
+        }
+        String data;
+            if (jedis.exists(key)) {
+                data = jedis.get(key);
+
+                if (data.length() > 0)
+                {
+                    jedis.set(key, "");
+
+                    writeLog("Read Metadata from Redis: "+data, Level.INFO);
+        
+                    sendChannelMetadata("auction", data);
+                }
+
+            }
     }
 
     private String getToken() {
@@ -156,12 +236,12 @@ public class AuctionThread extends java.lang.Thread {
             //@Override
             public void onSuccess(Void responseInfo) {
                 loginStatus = true;
-                System.out.println("login success!");
+                writeLog("login success!", Level.INFO);
             }
             //@Override
             public void onFailure(ErrorInfo errorInfo) {
                 loginStatus = false;
-                System.out.println("login failure!");
+                writeLog("login failure!", Level.SEVERE);
             }
         });
         return true;
@@ -182,25 +262,23 @@ public class AuctionThread extends java.lang.Thread {
         
     }
 
-
-
     private boolean joinChannel() {
         channelListener = new ChannelListener(channelId);
         mRtmChannel = mRtmClient.createChannel(channelId, channelListener);
         if (mRtmChannel == null) {
-            System.out.println("channel created failed!");
+            writeLog("channel created failed!", Level.INFO);
             return false;
         }
         mRtmChannel.join(new ResultCallback<Void>() {
             @Override
             public void onSuccess(Void responseInfo) {
-                System.out.println("join channel success!");
+                writeLog("join channel success!", Level.WARNING);
                 channelStatus = true;
             }
 
             @Override
             public void onFailure(ErrorInfo errorInfo) {
-                System.out.println("join channel failure! errorCode = " + errorInfo.getErrorCode());
+                writeLog("join channel failure! errorCode = " + errorInfo.getErrorCode(), Level.SEVERE);
                 channelStatus = false;
             }
         });
@@ -208,33 +286,90 @@ public class AuctionThread extends java.lang.Thread {
         return true;
     }
 
-    private void sendChannelMetadata(String key, String data) {
+    private void syncFirstTimeMetadata() {
+        HttpClient client = HttpClient.newBuilder().build();
 
-        RtmMetadata metadata = channelListener.metadata;
+        HttpRequest req = HttpRequest.newBuilder(URI.create(CONFIG.BASE_URL + "api/v1/auction?channelid=" + channelId)).build();
+        
+        writeLog("Sync metadata on Initialization Ready", Level.INFO);
 
-        for (RtmMetadataItem item : metadata.items) {
-            if (item.getKey().equals(key)) {
-                item.setValue(data);
-            }
+        try {
+            HttpResponse<String> resp = client.send(req, HttpResponse.BodyHandlers.ofString());
+            sendChannelMetadata("auction", resp.body());
         }
+        catch(Exception e)
+        {
+            writeLog("Sync init metadata failed", Level.SEVERE);
+        }
+    }
 
+    private void sendChannelMetadata(String key, String data) {
         RtmMetadataOptions options = new RtmMetadataOptions();
         options.setEnableRecordTs(true);
         options.setEnableRecordUserId(true);
 
-        System.out.println("Send Metadata start");
-        mRtmChannel.updateChannelMetadata(metadata.items, options, new ResultCallback<Void>() {
-            @Override
-            public void onSuccess(Void aVoid) {
-                System.out.println("Send Metadata Succeed");
+        RtmMetadata metadata = channelListener.metadata;
+
+        writeLog("Major revision:"+metadata.getMajorRevision(), Level.INFO);
+
+        boolean found = false;
+        if (metadata.items.isEmpty()) {
+            RtmMetadataItem item  = new RtmMetadataItem();
+            item.setKey(key);
+            item.setValue(data);
+            
+            metadata.items.add(item);   
+
+            mRtmChannel.addChannelMetadata(metadata.items, options, new ResultCallback<Void>() {
+                @Override
+                public void onSuccess(Void aVoid) {
+                    writeLog("add Metadata Succeed", Level.INFO);
+                }
+    
+                @Override
+                public void onFailure(ErrorInfo errorInfo) {
+                    final String errorCode = errorInfo.getErrorDescription();
+                    
+                    writeLog("Add Metadata to channel failed, erroCode = " + errorCode, Level.SEVERE);
+                }
+            });
+
+            return;
+        }
+        else {
+            for (RtmMetadataItem item : metadata.items) {
+                if (item.getKey().equals(key)) {
+                    item.setValue(data);
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                RtmMetadataItem item  = new RtmMetadataItem();
+                item.setKey(key);
+                item.setValue(data);
+                
+                metadata.items.add(item);     
             }
 
-            @Override
-            public void onFailure(ErrorInfo errorInfo) {
-                final int errorCode = errorInfo.getErrorCode();
-                System.out.println("Send Metadata to channel failed, erroCode = " + errorCode);
-            }
-        });
+            writeLog("Start Update Metadata", Level.INFO);
+            mRtmChannel.updateChannelMetadata(metadata.items, options, new ResultCallback<Void>() {
+                @Override
+                public void onSuccess(Void aVoid) {
+                    writeLog("Update Metadata Succeed", Level.INFO);
+                }
+
+                @Override
+                public void onFailure(ErrorInfo errorInfo) {
+                    final String errorCode = errorInfo.getErrorDescription();
+                    
+                    writeLog("Update Metadata to channel failed, erroCode = " + errorCode, Level.SEVERE);
+                }
+            });
+        }
+
+        
+
     } 
 }
 
@@ -262,14 +397,12 @@ class ChannelListener implements RtmChannelListener {
 
     @Override
     public void onMemberJoined(RtmChannelMember member) {
-        String account = member.getUserId();
-        System.out.println("member " + account + " joined the channel " + channel_);
+        
     }
 
     @Override
     public void onMemberLeft(RtmChannelMember member) {
-        String account = member.getUserId();
-        System.out.println("member " + account + " lefted the channel " + channel_);
+        
     }
 
     @Override
@@ -285,15 +418,12 @@ class ChannelListener implements RtmChannelListener {
 class ClientListener implements RtmClientListener {
     @Override
     public void onConnectionStateChanged(int state, int reason) {
-        System.out.println("on connection state changed to "
-            + state + " reason: " + reason);
+        
     }
 
     @Override
     public void onMessageReceived(RtmMessage rtmMessage, String peerId) {
-        String msg = rtmMessage.getText();
-        System.out.println("Receive message: " + msg 
-                    + " from " + peerId);
+        
     }
 
     @Override
